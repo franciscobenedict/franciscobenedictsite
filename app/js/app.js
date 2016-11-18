6e97fc9eb8594c89490fac6205369508f7fc0fb3
Francisco Benedict (acn) <franciscobenedict@gmail.com>
2016-11-18 14:34:39 +0000

    fixed footer and header

--- app/js/app.js
+++ app/js/app.js
@@ -6,12 +6,15 @@ fbApp.controller('AppCtrl', ['$scope', '$firebaseArray',
         // CREATE A FIREBASE REFERENCE
         var AppRef = new Firebase('https://franciscobenedict-1aa7b.firebaseio.com/');
         $scope.fbArray = $firebaseArray(AppRef);
+
         console.log('$scope.fbArray', $scope.fbArray);
         
         // $scope.welcome = $scope.fbArray[0][0];
         // console.log('$scope.welcome', $scope.welcome);
 
         // $scope.footerData 
+        
+        $scope.date = new Date();
     }
 ]);
 
