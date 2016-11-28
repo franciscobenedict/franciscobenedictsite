1e5efbf5ad4bedd80450c0751d73db98739c76d7
Francisco Benedict (acn) <franciscobenedict@gmail.com>
2016-11-28 23:57:46 +0000

    URLs updated; Page titles added; profile page updated;

--- app/js/grayscale.js
+++ app/js/grayscale.js
@@ -1,25 +1,15 @@
 //jQuery to collapse the navbar on scroll
-
-// $(function() {
-var parallax = $(".parallax"),
-    speed = 0.5;
-
-
 $(window).scroll(function() {
     $('body').scrollspy({ target: '.secondary-nav' });
     
     if ($(".navbar").length) {
+        // 25 pixels and beyond top of screen
         if ($(".navbar").offset().top > 25) {
-            // 50 pixels and beyond top of screen
             $(".navbar-fixed-top").addClass("top-nav-collapse");
             
             // Show the black version of the logo
             $('.white-logo').addClass('hidden');
             $('.grey-logo').removeClass('hidden');
-            
-            // Laguange links turns grey on hover
-            // $('#language a').addClass('green-link');
-            // $('#language a').removeClass('grey-link');
 
             // Shift secondary nav up
             $('.secondary-nav').addClass('shift-up');
@@ -31,42 +21,9 @@ $(window).scroll(function() {
             // Show the green version of the logo
             $('.white-logo').removeClass('hidden');
             $('.grey-logo').addClass('hidden');
-            
-            // // Laguange links turns green on hover
-            // $('#language a').removeClass('green-link');
-            // $('#language a').addClass('grey-link');
 
             // Shift secondary nav down
             $('.secondary-nav').removeClass('shift-up');
         }
     }
-
-    [].slice.call(parallax).forEach(function(el,i){
-        var windowYOffset = window.pageYOffset,
-        elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
-        el.style.backgroundPosition = elBackgrounPos;
-        console.log('el.style.backgroundPosition', el.style.backgroundPosition);
-    });
-
 });
-
-// // PARALLAX SCROLLING
-// (function(){
-
-//     console.log('PARALLAX SCROLLING LOADED');
-
-//     var parallax = document.querySelectorAll(".parallax"),
-//     speed = 0.5;
-
-//     window.onscroll = function(){
-//         [].slice.call(parallax).forEach(function(el,i){
-
-//             var windowYOffset = window.pageYOffset,
-//             elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
-
-//             el.style.backgroundPosition = elBackgrounPos;
-
-//         });
-//     };
-
-// })();
